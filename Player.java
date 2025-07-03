a1f2953b7844f43079e20d93f4462e6791a2f316
luk4tor <145482637+luk4tor@users.noreply.github.com>
2025-07-03 09:18:38 +0200

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,63 @@
+package entity;
+
+import main.GamePanel;
+import main.Keyhandler;
+
+public class Player extends Entity {
+
+  GamePanel gp;
+  KeyHandler keyH;
+
+  setDefaultValues();
+
+  public Player(GamePanel gp, Keyhandle keyH) {
+
+    this.gp = gp;
+    this.keyH = keyH;
+  }
+
+  public void setDefaultValues() {
+
+    x = 100;
+    y = 100;
+    speed = 4;
+  }
+
+  public void update() {
+
+    if(keyH.upPressed == true) {
+      y -= speed;
+    }
+    if(keyH.downPressed == true) {
+      y += speed;
+    }
+    if(keyH.leftPressed == true) {
+      x -= speed;
+    }
+    if(keyH.rightPressed == true) {
+      x += speed;
+    }
+
+  }
+
+  public void draw() {
+
+    g2.setColor(Color.white);
+    g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
