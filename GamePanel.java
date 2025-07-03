e130b4810da5f477a01a22911a5f05ae15b485c7
AnnaDittmar <anna-maria.dittmar@gmx.de>
2025-07-03 09:16:57 +0200

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -5,14 +5,14 @@ import java.awt.Color;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel{
+public class GamePanel extends JPanel implements Runnable{
   //ist wie ein Spielbildschirm
   //SCREEN SETTINGS
   final int originalTileSize = 16;
   //Originalgröße einzelner Bausteine, für moderne Bildschirme etwas klein
   final int scale = 3;
   //Die Bausteine wirken größer(48*48)
-  final int tileSize = originalTileSize * scale;
+  public final int tileSize = originalTileSize * scale;
   //erzeugt einen Retro Look
   final int maxScreenCol = 16;
   //Anzahl der Bausteine die nebeneinander sind 
@@ -23,6 +23,9 @@ public class GamePanel extends JPanel{
   final screenHeight = tileSize * maxScreenRow;
   //Höhe des Bildschirms = 576 pixel
 
+  Thread gameThread;
+  //hilft dabei wiederholende Sequenzen einzustellen, wie Charakter Animationen
+
 
   public GamePanel(){
     this.setPreferredSize(new Dimension(screenWidth, screenHeight);
@@ -32,6 +35,20 @@ public class GamePanel extends JPanel{
     this.setDoubleBuffered(true);
     //Er malt im Hintergrund hilft mit rendering performance
     
+  }
+
+  public void startGameThread(){
+    gameThread = new Thread(this);
+    //sorgt dafür, dass die GamePanel-Klasse zu Thread hinzugefügt wird
+    gameThread.start();
+  }
+
+  @Override
+  public void run(){
+    //erzeugt einen GameLoop der Zentrum des Spiels ist
+    while(gameThread != null){
+      //solange es gameThread gibt, wird wiederholt
+    }
   }
 
 }
